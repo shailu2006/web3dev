39a67576986a5887ed83c011955fc94251331595
Shailesh <shailu2006@gmail.com>
2022-07-20 14:54:21 +0530

    FundMe contract with Hardhat deployable

---
+++ hardhat-fund-me/deploy/01-deploy-fund-me.js
@@ -0,0 +1,39 @@
+const { network } = require("hardhat");
+const {
+  networkConfig,
+  developmentChains,
+} = require("../helper-hardhat-config");
+const { verify } = require("../utils/verify");
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+  const { deploy, log } = deployments;
+  const { deployer } = await getNamedAccounts();
+  const chainId = network.config.chainId;
+
+  let ethUsdPriceFeedAddress;
+  // if an contract doesn't exist, we deploy a minimal version of it
+  // for our local testing
+  if (developmentChains.includes(network.name)) {
+    const ethUsdAggregator = await deployments.get("MockV3Aggregator");
+    ethUsdPriceFeedAddress = ethUsdAggregator.address;
+  } else {
+    ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"];
+  }
+  log("----------------------------------------------------");
+  log("Deploying FundMe and waiting for confirmations...");
+  const args = [ethUsdPriceFeedAddress];
+  const fundMe = await deploy("FundMe", {
+    from: deployer,
+    args: args,
+    log: true,
+    // we need to wait if on a live network so we can verify properly
+    waitConfirmations: network.config.blockConfirmations || 1,
+  });
+
+  // Actual Deployment
+  if (!developmentChains.includes(network.name)) {
+    await verify(fundMe.address, args);
+  }
+};
+
+module.exports.tags = ["all", "fundme"];
